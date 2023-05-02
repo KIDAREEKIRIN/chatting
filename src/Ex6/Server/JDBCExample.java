a836a98104a3b9f7e271846a4c9e1022f57d370e
keonwookim <walk000183@gmail.com>
2023-05-02 16:06:18 +0900

    23.05.02.(화) - 자바 JDBC 연결 이후, 주석 달기

--- src/Ex6/Server/JDBCExample.java
+++ src/Ex6/Server/JDBCExample.java
@@ -6,25 +6,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class JDBCExample {
+public class JDBCExample { // 시작.
     public static void main(String[] args) {
-        Connection connection = null;
-        Statement statement = null;
+        Connection connection = null; //    데이터베이스와 연결을 위한 객체.
+        Statement statement = null; //  sql문을 실행하기 위한 객체.
         ResultSet resultSet = null;
 
         try {
-            String url = "jdbc:mysql://3.37.249.79:3306/test5"; // test5는 데이터베이스 이름
-            String user = "test";
-            String password = "test";
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://3.37.249.79:3306/test5"; // test5는 데이터베이스 이름 //
+            String user = "test"; // user 이름.
+            String password = "test"; // 비밀번호.
+            Class.forName("com.mysql.cj.jdbc.Driver"); // 드라이버 로딩
             // 드라이버 버전 확인
 //            System.out.println("JDBC Driver Version: " + DriverManager.getDriver("jdbc:mysql://3.37.249.79:3306/test5").getMajorVersion() + "." + DriverManager.getDriver("jdbc:mysql://3.37.249.79:3306/test5").getMinorVersion());
             System.out.println("url" + url);
             System.out.println("user" + user);
             System.out.println("password" + password);
-            connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, user, password); //
 
-            statement = connection.createStatement();
+            statement = connection.createStatement(); //
             resultSet = statement.executeQuery("SELECT * FROM default_duty_name");
 
             while(resultSet.next()) { //
